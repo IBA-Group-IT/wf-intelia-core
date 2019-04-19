ab297f7bfe40693e1a29d79d5a6a93d390807f86
Alexander Dmitriev <dmitriev@ibagroup.eu>
2019-04-19 17:47:24 +0300

    92 migration preps - machine use extracted into bridge and machiversionadaptations classes, pom set to profiles

--- src/main/java/com/ibagroup/wf/intelia/core/security/SecureEntryDtoWrapper.java
+++ src/main/java/com/ibagroup/wf/intelia/core/security/SecureEntryDtoWrapper.java
@@ -1,75 +1,109 @@
 package com.ibagroup.wf.intelia.core.security;
 
+import java.util.Date;
 import com.drew.lang.annotations.SuppressWarnings;
-import com.freedomoss.crowdcontrol.webharvest.web.dto.SecureEntryDTO;
+import com.ibagroup.wf.intelia.compatibility.ISecureEntryDtoWrapper;
 
-public class SecureEntryDtoWrapper extends SecureEntryDTO {
+public class SecureEntryDtoWrapper {
 
-    public SecureEntryDtoWrapper() {
-        super();
-    }
+	public static ISecureEntryDtoWrapper<SecureEntryDtoWrapper> WRAPPER = (a, k, v, d) -> new SecureEntryDtoWrapper(a, k, v, d);
 
-    public SecureEntryDtoWrapper(String alias) {
-        super(alias);
-    }
+	public String alias;
+	public String key;
+	public String value;
+	public Date lastUpdateDate;
 
-    public SecureEntryDtoWrapper(SecureEntryDTO dto) {
-        setAlias(dto.getAlias());
-        setKey(dto.getKey());
-        setLastUpdateDate(dto.getLastUpdateDate());
-        setValue(dto.getValue());
-    }
+	public SecureEntryDtoWrapper() {
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getAlias() == null) ? 0 : getAlias().hashCode());
-        result = prime * result + ((getKey() == null) ? 0 : getKey().hashCode());
-        result = prime * result + ((getValue() == null) ? 0 : getValue().hashCode());
-        return result;
-    }
+	public SecureEntryDtoWrapper(String alias, String key, String value, Date lastUpdateDate) {
+		this.alias = alias;
+		this.key = key;
+		this.value = value;
+		this.lastUpdateDate = lastUpdateDate;
+	}
 
-    @SuppressWarnings(value = "BC_EQUALS_METHOD_SHOULD_WORK_FOR_ALL_OBJECTS",
-            justification = "This class can be used in groove webharvest config. And as webharvest can use different classloaders for each groovy scripts inside one config, instance of can return false for vene same object")
+	public String getAlias() {
+		return this.alias;
+	}
 
-    @Override
-    public boolean equals(Object second) {
-        if (second == null) {
-            return false;
-        }
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
 
-        SecureEntryDTO obj = (SecureEntryDTO) second;
+	public String getKey() {
+		return this.key;
+	}
 
-        if (getAlias() == null) {
-            if (obj.getAlias() != null) {
-                return false;
-            }
-        } else if (!getAlias().equals(obj.getAlias())) {
-            return false;
-        }
+	public void setKey(String key) {
+		this.key = key;
+	}
 
-        if (getKey() == null) {
-            if (obj.getKey() != null) {
-                return false;
-            }
-        } else if (!getKey().equals(obj.getKey())) {
-            return false;
-        }
+	public String getValue() {
+		return this.value;
+	}
 
-        if (getValue() == null) {
-            if (obj.getValue() != null) {
-                return false;
-            }
-        } else if (!getValue().equals(obj.getValue())) {
-            return false;
-        }
+	public void setValue(String value) {
+		this.value = value;
+	}
 
-        return true;
-    }
+	public Date getLastUpdateDate() {
+		return this.lastUpdateDate;
+	}
 
-    @Override
-    public String toString() {
-        return "SecureEntryDtoWrapper - " + super.toString();
-    };
+	public void setLastUpdateDate(Date lastUpdateDate) {
+		this.lastUpdateDate = lastUpdateDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((getAlias() == null) ? 0 : getAlias().hashCode());
+		result = prime * result + ((getKey() == null) ? 0 : getKey().hashCode());
+		result = prime * result + ((getValue() == null) ? 0 : getValue().hashCode());
+		return result;
+	}
+
+	@SuppressWarnings(value = "BC_EQUALS_METHOD_SHOULD_WORK_FOR_ALL_OBJECTS", justification = "This class can be used in groove webharvest config. And as webharvest can use different classloaders for each groovy scripts inside one config, instance of can return false for vene same object")
+
+	@Override
+	public boolean equals(Object second) {
+		if (second == null) {
+			return false;
+		}
+
+		SecureEntryDtoWrapper obj = (SecureEntryDtoWrapper) second;
+
+		if (getAlias() == null) {
+			if (obj.getAlias() != null) {
+				return false;
+			}
+		} else if (!getAlias().equals(obj.getAlias())) {
+			return false;
+		}
+
+		if (getKey() == null) {
+			if (obj.getKey() != null) {
+				return false;
+			}
+		} else if (!getKey().equals(obj.getKey())) {
+			return false;
+		}
+
+		if (getValue() == null) {
+			if (obj.getValue() != null) {
+				return false;
+			}
+		} else if (!getValue().equals(obj.getValue())) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "SecureEntryDtoWrapper [alias=" + this.alias + ", lastUpdateDate=" + this.lastUpdateDate + "]";
+	}
+
 }
---
+++ src/main/java/com/ibagroup/wf/intelia/core/security/WfSecurityBridge.java
@@ -0,0 +1,105 @@
+package com.ibagroup.wf.intelia.core.security;
+
+import static com.ibagroup.wf.intelia.core.security.SecureEntryDtoWrapper.WRAPPER;
+import static com.ibagroup.wf.intelia.compatibility.MachineVersionAdaptations.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.ContextLoader;
+
+import com.freedomoss.crowdcontrol.webharvest.WebHarvestConstants;
+import com.freedomoss.crowdcontrol.webharvest.plugin.security.provider.ISecureEntryProvider;
+import com.freedomoss.crowdcontrol.webharvest.plugin.security.provider.SecureStoreProvider;
+import com.freedomoss.crowdcontrol.webharvest.web.WebServiceConnectionProperties;
+import com.ibagroup.wf.intelia.core.BindingUtils;
+import com.workfusion.service.CachedWebServiceFactory;
+import com.workfusion.utils.security.Credentials;
+
+import groovy.lang.Binding;
+
+/**
+ * Bridge to Workfusion Machine Security features.
+ * <ul>
+ * <li>Hides internal machine guts and exposes machine-agnostic entities</li>
+ * <li>Should not be used outside core</li>
+ * <li>Should import only 'stable' WF classes that does not change across machine versions</li>
+ * </ul>
+ * 
+ * @author dmitriev
+ *
+ */
+public class WfSecurityBridge {
+
+	private static final Logger logger = LoggerFactory.getLogger(WfSecurityBridge.class);
+
+	private Binding binding = null;
+
+	/**
+	 * @param binding
+	 *            - Binding implementation, usually from webharvest config
+	 */
+	public WfSecurityBridge(Binding binding) {
+		super();
+		this.binding = binding;
+	}
+
+	public SecureEntryDtoWrapper getUserSecureEntry(String providerId, String alias) {
+		Map<?, ?> securityProviderMap = (Map<?, ?>) BindingUtils.getWrappedObjFromContext(binding, WebHarvestConstants.SECURITY_PROVIDER_MAP);
+
+		ISecureEntryProvider entryProvider = securityProviderMap == null ? null : (ISecureEntryProvider) securityProviderMap.get(providerId);
+
+		if (entryProvider == null) {
+			entryProvider = new SecureStoreProvider(getService(getISecureStoreServiceClass()));
+		}
+		logger.info("entryProvider: " + entryProvider.toString());
+		Map<String, Object> params = new HashMap<>();
+		params.put(ISecureEntryProvider.PARAM_ALIAS, alias);
+
+		SecureEntryDtoWrapper entry = wrap(entryProvider.getUserSecureEntry(params), WRAPPER);
+
+		try {
+			if (entry != null) {
+				entry = wrap(getServiceFromWebAppContext(getISecureStoreServiceClass()).getEntry(alias), WRAPPER);
+			}
+		} catch (Throwable e) {
+			logger.error(e.toString());
+		}
+		return entry;
+	}
+
+	/**
+	 * Update the secured storage entry
+	 * 
+	 * @param alias
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public boolean updateEntry(String alias, String key, String value) {
+		boolean isSuccessful = true;
+		isSuccessful = getService(getISecureStoreServiceClass()).updateEntry(alias, key, value);
+		logger.info("- SecureStorageUpdate - isSaveSuccessful = {} ", isSuccessful);
+
+		return isSuccessful;
+	}
+
+	private <T> T getServiceFromWebAppContext(Class<T> claz) {
+		return ContextLoader.getCurrentWebApplicationContext().getBean(claz);
+	}
+
+	private <T> T getService(Class<T> claz) {
+		WebServiceConnectionProperties connectionProperties = new WebServiceConnectionProperties();
+		Credentials credentials = BindingUtils.getUserCredentials(binding);
+		connectionProperties.setCredentials(credentials);
+
+		String applicationHostString = BindingUtils.getApplicationHost(binding);
+		connectionProperties.setWorkfusionHost(applicationHostString);
+		String contextPathString = BindingUtils.getApplicationContextPath(binding);
+		connectionProperties.setWorkfusionUrl(contextPathString);
+		return (T) CachedWebServiceFactory.getInstance().getOrCreateSecureStoreService(connectionProperties);
+	}
+
+}
