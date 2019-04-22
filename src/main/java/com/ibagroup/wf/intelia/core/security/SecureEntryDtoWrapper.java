5e128aa2f89b6d2e66e53170f3e9d833a3bee998
Alexander Dmitriev <dmitriev@ibagroup.eu>
2019-04-22 14:16:49 +0300

    google code formater applied

--- src/main/java/com/ibagroup/wf/intelia/core/security/SecureEntryDtoWrapper.java
+++ src/main/java/com/ibagroup/wf/intelia/core/security/SecureEntryDtoWrapper.java
@@ -6,104 +6,104 @@ import com.ibagroup.wf.intelia.compatibility.ISecureEntryDtoWrapper;
 
 public class SecureEntryDtoWrapper {
 
-	public static ISecureEntryDtoWrapper<SecureEntryDtoWrapper> WRAPPER = (a, k, v, d) -> new SecureEntryDtoWrapper(a, k, v, d);
+    public static ISecureEntryDtoWrapper<SecureEntryDtoWrapper> WRAPPER = (a, k, v, d) -> new SecureEntryDtoWrapper(a, k, v, d);
 
-	public String alias;
-	public String key;
-	public String value;
-	public Date lastUpdateDate;
+    public String alias;
+    public String key;
+    public String value;
+    public Date lastUpdateDate;
 
-	public SecureEntryDtoWrapper() {
-	}
+    public SecureEntryDtoWrapper() {}
 
-	public SecureEntryDtoWrapper(String alias, String key, String value, Date lastUpdateDate) {
-		this.alias = alias;
-		this.key = key;
-		this.value = value;
-		this.lastUpdateDate = lastUpdateDate;
-	}
+    public SecureEntryDtoWrapper(String alias, String key, String value, Date lastUpdateDate) {
+        this.alias = alias;
+        this.key = key;
+        this.value = value;
+        this.lastUpdateDate = lastUpdateDate == null ? null:new Date(lastUpdateDate.getTime());
+    }
 
-	public String getAlias() {
-		return this.alias;
-	}
+    public String getAlias() {
+        return this.alias;
+    }
 
-	public void setAlias(String alias) {
-		this.alias = alias;
-	}
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
 
-	public String getKey() {
-		return this.key;
-	}
+    public String getKey() {
+        return this.key;
+    }
 
-	public void setKey(String key) {
-		this.key = key;
-	}
+    public void setKey(String key) {
+        this.key = key;
+    }
 
-	public String getValue() {
-		return this.value;
-	}
+    public String getValue() {
+        return this.value;
+    }
 
-	public void setValue(String value) {
-		this.value = value;
-	}
+    public void setValue(String value) {
+        this.value = value;
+    }
 
-	public Date getLastUpdateDate() {
-		return this.lastUpdateDate;
-	}
+    public Date getLastUpdateDate() {
+        return lastUpdateDate == null ? null:new Date(lastUpdateDate.getTime());
+    }
 
-	public void setLastUpdateDate(Date lastUpdateDate) {
-		this.lastUpdateDate = lastUpdateDate;
-	}
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate == null ? null:new Date(lastUpdateDate.getTime());
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((getAlias() == null) ? 0 : getAlias().hashCode());
-		result = prime * result + ((getKey() == null) ? 0 : getKey().hashCode());
-		result = prime * result + ((getValue() == null) ? 0 : getValue().hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getAlias() == null) ? 0 : getAlias().hashCode());
+        result = prime * result + ((getKey() == null) ? 0 : getKey().hashCode());
+        result = prime * result + ((getValue() == null) ? 0 : getValue().hashCode());
+        return result;
+    }
 
-	@SuppressWarnings(value = "BC_EQUALS_METHOD_SHOULD_WORK_FOR_ALL_OBJECTS", justification = "This class can be used in groove webharvest config. And as webharvest can use different classloaders for each groovy scripts inside one config, instance of can return false for vene same object")
+    @SuppressWarnings(value = "BC_EQUALS_METHOD_SHOULD_WORK_FOR_ALL_OBJECTS",
+            justification = "This class can be used in groove webharvest config. And as webharvest can use different classloaders for each groovy scripts inside one config, instance of can return false for vene same object")
 
-	@Override
-	public boolean equals(Object second) {
-		if (second == null) {
-			return false;
-		}
+    @Override
+    public boolean equals(Object second) {
+        if (second == null) {
+            return false;
+        }
 
-		SecureEntryDtoWrapper obj = (SecureEntryDtoWrapper) second;
+        SecureEntryDtoWrapper obj = (SecureEntryDtoWrapper) second;
 
-		if (getAlias() == null) {
-			if (obj.getAlias() != null) {
-				return false;
-			}
-		} else if (!getAlias().equals(obj.getAlias())) {
-			return false;
-		}
+        if (getAlias() == null) {
+            if (obj.getAlias() != null) {
+                return false;
+            }
+        } else if (!getAlias().equals(obj.getAlias())) {
+            return false;
+        }
 
-		if (getKey() == null) {
-			if (obj.getKey() != null) {
-				return false;
-			}
-		} else if (!getKey().equals(obj.getKey())) {
-			return false;
-		}
+        if (getKey() == null) {
+            if (obj.getKey() != null) {
+                return false;
+            }
+        } else if (!getKey().equals(obj.getKey())) {
+            return false;
+        }
 
-		if (getValue() == null) {
-			if (obj.getValue() != null) {
-				return false;
-			}
-		} else if (!getValue().equals(obj.getValue())) {
-			return false;
-		}
-		return true;
-	}
+        if (getValue() == null) {
+            if (obj.getValue() != null) {
+                return false;
+            }
+        } else if (!getValue().equals(obj.getValue())) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "SecureEntryDtoWrapper [alias=" + this.alias + ", lastUpdateDate=" + this.lastUpdateDate + "]";
-	}
+    @Override
+    public String toString() {
+        return "SecureEntryDtoWrapper [alias=" + this.alias + ", lastUpdateDate=" + this.lastUpdateDate + "]";
+    }
 
 }
--- src/main/java/com/ibagroup/wf/intelia/core/security/SecurityUtils.java
+++ src/main/java/com/ibagroup/wf/intelia/core/security/SecurityUtils.java
@@ -6,15 +6,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.ibagroup.wf.intelia.core.CommonConstants;
 import com.ibagroup.wf.intelia.core.datastore.DataStoreQuery;
 import com.ibagroup.wf.intelia.core.datastore.DataStoreQuery.RowItem;
-
 import groovy.lang.Binding;
 
 /**
@@ -28,169 +25,166 @@ import groovy.lang.Binding;
  */
 public class SecurityUtils {
 
-	private static final Logger logger = LoggerFactory.getLogger(SecurityUtils.class);
+    private static final Logger logger = LoggerFactory.getLogger(SecurityUtils.class);
 
-	private static class AliasDs {
-		private String appName;
-		private String aliasName;
+    private static class AliasDs {
+        private String appName;
+        private String aliasName;
 
-		private AliasDs() {
-			super();
-		}
+        private AliasDs() {
+            super();
+        }
 
-		public void setAppName(String appName) {
-			this.appName = appName;
-		}
+        public void setAppName(String appName) {
+            this.appName = appName;
+        }
 
-		public String getAppName() {
-			return appName;
-		}
+        public String getAppName() {
+            return appName;
+        }
 
-		public void setAliasName(String aliasName) {
-			this.aliasName = aliasName;
-		}
+        public void setAliasName(String aliasName) {
+            this.aliasName = aliasName;
+        }
 
-		public String getAliasName() {
-			return aliasName;
-		}
+        public String getAliasName() {
+            return aliasName;
+        }
 
-		@Override
-		public String toString() {
-			return "AliasDs [appName=" + appName + ", aliasName=" + aliasName + "]";
-		}
-	}
+        @Override
+        public String toString() {
+            return "AliasDs [appName=" + appName + ", aliasName=" + aliasName + "]";
+        }
+    }
 
-	private List<SecureEntryDtoWrapper> secDs = new ArrayList<>();
-	private List<AliasDs> aliasDs = null;
+    private List<SecureEntryDtoWrapper> secDs = new ArrayList<>();
+    private List<AliasDs> aliasDs = null;
 
-	private DataStoreQuery dataStoreAccess;
+    private DataStoreQuery dataStoreAccess;
 
-	private WfSecurityBridge wfSecurityBridge;
+    private WfSecurityBridge wfSecurityBridge;
 
-	private Binding binding;
+    private Binding binding;
 
-	/**
-	 * @param binding
-	 *            - Binding implementation, usually from webharvest config
-	 */
-	public SecurityUtils(Binding binding) {
-		this.binding = binding;
-		dataStoreAccess = new DataStoreQuery(binding);
-		try {
-			secDs = dataStoreAccess.executeQuery("SecureDataStore", "select * from @this;").getSelectResultAsListRows().get().stream().map(row -> {
-				return parseSec(row);
-			}).collect(Collectors.toList());
+    /**
+     * @param binding - Binding implementation, usually from webharvest config
+     */
+    public SecurityUtils(Binding binding) {
+        this.binding = binding;
+        dataStoreAccess = new DataStoreQuery(binding);
+        try {
+            secDs = dataStoreAccess.executeQuery("SecureDataStore", "select * from @this;").getSelectResultAsListRows().get().stream().map(row -> {
+                return parseSec(row);
+            }).collect(Collectors.toList());
 
-		} catch (Exception exp) {
-			logger.info("SecureDataStore data store exists only localy");
-		}
+        } catch (Exception exp) {
+            logger.info("SecureDataStore data store exists only localy");
+        }
 
-		this.aliasDs = dataStoreAccess.executeQuery("UserAliasesPerApplication", "select * from @this where status = '" + CommonConstants.INACTIVE + "';").getSelectResultAsListRows().get().stream()
-				.map(row -> {
-					AliasDs aDs = new AliasDs();
-					for (Iterator<RowItem> iterator = row.iterator(); iterator.hasNext();) {
-						RowItem item = iterator.next();
-						switch (item.getColumn()) {
-						case "app_alias":
-							aDs.setAppName(item.getValue());
-							break;
-						case "security_alias":
-							aDs.setAliasName(item.getValue());
-							break;
-						default:
-							break;
-						}
-					}
+        this.aliasDs = dataStoreAccess.executeQuery("UserAliasesPerApplication", "select * from @this where status = '" + CommonConstants.INACTIVE + "';")
+                .getSelectResultAsListRows().get().stream().map(row -> {
+                    AliasDs aDs = new AliasDs();
+                    for (Iterator<RowItem> iterator = row.iterator(); iterator.hasNext();) {
+                        RowItem item = iterator.next();
+                        switch (item.getColumn()) {
+                            case "app_alias":
+                                aDs.setAppName(item.getValue());
+                                break;
+                            case "security_alias":
+                                aDs.setAliasName(item.getValue());
+                                break;
+                            default:
+                                break;
+                        }
+                    }
 
-					return aDs;
-				}).collect(Collectors.toList());
-	}
+                    return aDs;
+                }).collect(Collectors.toList());
+    }
 
-	/**
-	 * @param appName
-	 *            - alias name to look up on datastore aliasDs
-	 * @return return List of SecureEntryDtoWrapper for security aliases stored in aliasDs for given application alias aliasName
-	 */
-	public List<SecureEntryDtoWrapper> getListOfEntriesByAppName(String appName) {
-		if (StringUtils.isBlank(appName)) {
-			throw new IllegalArgumentException("Application alias name can't be empty");
-		}
+    /**
+     * @param appName - alias name to look up on datastore aliasDs
+     * @return return List of SecureEntryDtoWrapper for security aliases stored in aliasDs for given
+     *         application alias aliasName
+     */
+    public List<SecureEntryDtoWrapper> getListOfEntriesByAppName(String appName) {
+        if (StringUtils.isBlank(appName)) {
+            throw new IllegalArgumentException("Application alias name can't be empty");
+        }
 
-		List<SecureEntryDtoWrapper> result = aliasDs.stream().filter((AliasDs aDs) -> appName.equalsIgnoreCase(aDs.getAppName()) ? true : false)
-				.map((AliasDs aDs) -> getSecureEntry(aDs.getAliasName())).collect(Collectors.toList());
-		logger.info("Users list: " + result.toString());
-		return result;
-	}
+        List<SecureEntryDtoWrapper> result = aliasDs.stream().filter((AliasDs aDs) -> appName.equalsIgnoreCase(aDs.getAppName()) ? true : false)
+                .map((AliasDs aDs) -> getSecureEntry(aDs.getAliasName())).collect(Collectors.toList());
+        logger.info("Users list: " + result.toString());
+        return result;
+    }
 
-	/**
-	 * @param appName
-	 *            - appName for creds
-	 * @return random SecureEntryDtoWrapper for given alias
-	 */
-	public SecureEntryDtoWrapper getAnyCred(String appName) {
-		return getListOfEntriesByAppName(appName).stream().findAny().orElseGet(null);
-	}
+    /**
+     * @param appName - appName for creds
+     * @return random SecureEntryDtoWrapper for given alias
+     */
+    public SecureEntryDtoWrapper getAnyCred(String appName) {
+        return getListOfEntriesByAppName(appName).stream().findAny().orElseGet(null);
+    }
 
-	/**
-	 * @param aliasString
-	 *            - alias name in secure data store
-	 * @return return Item from Security Storage for specific <code>aliasString</code>
-	 */
-	public SecureEntryDtoWrapper getSecureEntry(String aliasString) {
-		logger.info("Trying to get securityDTO for: " + aliasString);
+    /**
+     * @param aliasString - alias name in secure data store
+     * @return return Item from Security Storage for specific <code>aliasString</code>
+     */
+    public SecureEntryDtoWrapper getSecureEntry(String aliasString) {
+        logger.info("Trying to get securityDTO for: " + aliasString);
 
-		return secDs.stream().filter((sec) -> sec.getAlias().equalsIgnoreCase(aliasString)).findAny().orElseGet(() -> {
-			return getWfSecurityBridge().getUserSecureEntry(null, aliasString);
-		});
+        return secDs.stream().filter((sec) -> sec.getAlias().equalsIgnoreCase(aliasString)).findAny().orElseGet(() -> {
+            return getWfSecurityBridge().getUserSecureEntry(null, aliasString);
+        });
 
-	}
+    }
 
-	/**
-	 * Update status column in UserAliasesPerApplication data store.
-	 *
-	 * @param appAlias
-	 * @param securityAlias
-	 * @param status
-	 */
-	public void updateUserAliasesPerApplication(String appAlias, String securityAlias, String status) {
-		String updateQuery = "UPDATE @this SET status =\'" + status + "\' WHERE app_alias =\'" + appAlias + "\' and security_alias=\'" + securityAlias + "\'";
-		dataStoreAccess.executeQuery("UserAliasesPerApplication", updateQuery);
-	}
+    /**
+     * Update status column in UserAliasesPerApplication data store.
+     *
+     * @param appAlias
+     * @param securityAlias
+     * @param status
+     */
+    public void updateUserAliasesPerApplication(String appAlias, String securityAlias, String status) {
+        String updateQuery = "UPDATE @this SET status =\'" + status + "\' WHERE app_alias =\'" + appAlias + "\' and security_alias=\'" + securityAlias + "\'";
+        dataStoreAccess.executeQuery("UserAliasesPerApplication", updateQuery);
+    }
 
-	private SecureEntryDtoWrapper parseSec(List<RowItem> rowList) {
-		SecureEntryDtoWrapper secResult = new SecureEntryDtoWrapper();
-		for (Iterator<RowItem> iterator = rowList.iterator(); iterator.hasNext();) {
-			RowItem item = iterator.next();
-			switch (item.getColumn()) {
-			case "Alias":
-				secResult.setAlias(item.getValue());
-				break;
-			case "Key":
-				secResult.setKey(item.getValue());
-				break;
-			case "Value":
-				secResult.setValue(item.getValue());
-				break;
-			case "Last_Update_Date":
-				secResult.setLastUpdateDate(new SimpleDateFormat("MM.dd.yyyy HH:mm").parse(item.getValue(), new ParsePosition(0)));
-				break;
-			default:
-				break;
-			}
-		}
+    private SecureEntryDtoWrapper parseSec(List<RowItem> rowList) {
+        SecureEntryDtoWrapper secResult = new SecureEntryDtoWrapper();
+        for (Iterator<RowItem> iterator = rowList.iterator(); iterator.hasNext();) {
+            RowItem item = iterator.next();
+            switch (item.getColumn()) {
+                case "Alias":
+                    secResult.setAlias(item.getValue());
+                    break;
+                case "Key":
+                    secResult.setKey(item.getValue());
+                    break;
+                case "Value":
+                    secResult.setValue(item.getValue());
+                    break;
+                case "Last_Update_Date":
+                    secResult.setLastUpdateDate(new SimpleDateFormat("MM.dd.yyyy HH:mm").parse(item.getValue(), new ParsePosition(0)));
+                    break;
+                default:
+                    break;
+            }
+        }
 
-		return secResult;
-	}
+        return secResult;
+    }
 
-	public WfSecurityBridge getWfSecurityBridge() {
-		if (wfSecurityBridge == null) {
-			wfSecurityBridge = new WfSecurityBridge(binding);
-		}
-		return wfSecurityBridge;
-	}
+    public WfSecurityBridge getWfSecurityBridge() {
+        if (wfSecurityBridge == null) {
+            wfSecurityBridge = new WfSecurityBridge(binding);
+        }
+        return wfSecurityBridge;
+    }
 
-	public void setWfSecurityBridge(WfSecurityBridge wfSecurityBridge) {
-		this.wfSecurityBridge = wfSecurityBridge;
-	}
+    public void setWfSecurityBridge(WfSecurityBridge wfSecurityBridge) {
+        this.wfSecurityBridge = wfSecurityBridge;
+    }
 
 }
