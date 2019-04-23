bbbd5ffc5cef699950a8f41ebdfba8bfe532502d
Igor Zhukov <ihar.zhukau@standardbank.co.za>
2019-04-23 11:08:00 +0300

    update formatting

--- src/main/java/com/ibagroup/wf/intelia/compatibility/ISecureEntryDtoWrapper.java
+++ src/main/java/com/ibagroup/wf/intelia/compatibility/ISecureEntryDtoWrapper.java
@@ -4,5 +4,5 @@ import java.util.Date;
 
 @FunctionalInterface
 public interface ISecureEntryDtoWrapper<T> {
-	T wrap(String alias, String key, String value, Date lastUpdateDate);
+    T wrap(String alias, String key, String value, Date lastUpdateDate);
 }
