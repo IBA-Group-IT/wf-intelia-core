05b8ebe2fad5fc542b676d3233646a672e4ca8c2
Mikhail Trubach <mtrubatch@iba.by>
2019-08-15 10:09:32 +0300

    IWF-35. Add common page object functionality.

---
+++ src/main/java/com/ibagroup/wf/intelia/core/system/ChromePageObject.java
@@ -0,0 +1,81 @@
+package com.ibagroup.wf.intelia.core.system;
+
+import org.apache.commons.io.FilenameUtils;
+import org.openqa.selenium.WebElement;
+
+import com.ibagroup.wf.intelia.core.config.ConfigurationManager;
+import com.workfusion.rpa.helpers.Script;
+import com.workfusion.rpa.helpers.utils.ApiUtils;
+
+import groovy.lang.Binding;
+
+public class ChromePageObject extends WebPageObject {
+
+	public ChromePageObject(Binding binding, ConfigurationManager cmn) {
+		super(binding, cmn);
+	}
+
+	public String clickAndDownload(WebElement downloadLink) {
+		String lastDownloadedFile = getLastDownloadedFileOnAgent();
+		scrollToElement(downloadLink);
+		downloadLink.click();
+		String filePath = waitUntilFileDownloadedOnAgent(lastDownloadedFile);
+		return filePath;
+	}
+
+	private String getLastDownloadedFileOnAgent() {
+		StringBuilder script = new StringBuilder();
+		script.append("def userHomeDir = System.getProperty('user.home');\n");
+		script.append("def dir = new File(userHomeDir + '/Downloads');\n");
+		script.append("def files = dir.listFiles();\n");
+		script.append("def lastModifiedFile = files && files.size() > 0 ? files.sort { -it.lastModified() }.head() : null;\n");
+		script.append("return lastModifiedFile?.getAbsolutePath();");
+
+		String result = (String) Script.executeGroovyScript(script.toString());
+
+		return result;
+	}
+
+	private String waitUntilFileDownloadedOnAgent(String lastDownloadedFile) {
+		String lastDownloadedFileName = lastDownloadedFile != null ? ApiUtils.encodeFilePathForGroovyScript(FilenameUtils.getName(lastDownloadedFile)) : "";
+
+		StringBuilder script = new StringBuilder();
+		script.append("def getLastModifiedFileName = {\n");
+		script.append("		def userHomeDir = System.getProperty('user.home');\n");
+		script.append("		def dir = new File(userHomeDir + '/Downloads');\n");
+		script.append("		def files = dir.listFiles();\n");
+		script.append("		def lastModifiedFile = files && files.size() > 0 ? files.sort { -it.lastModified() }.head() : null;\n");
+		script.append("		return lastModifiedFile?.getAbsolutePath();\n");
+		script.append("}\n");
+		script.append("\n");
+		script.append("def downloadFilePath = getLastModifiedFileName();\n");
+		script.append("def maxCounter = 100;\n");
+		script.append("while ((downloadFilePath == null || org.apache.commons.io.FilenameUtils.getName(downloadFilePath).equals(" + lastDownloadedFileName + ")) && maxCounter-- > 0) {\n");
+		script.append("		Thread.sleep(500);\n");
+		script.append("		downloadFilePath = getLastModifiedFileName();\n");
+		script.append("}\n");
+		script.append("if (maxCounter > 0) {\n");
+		script.append("		maxCounter = 2*60*10;\n");
+		script.append("		downloadFilePath = getLastModifiedFileName();\n");
+		script.append("		def fileExtension = org.apache.commons.io.FilenameUtils.getExtension(downloadFilePath);\n");
+		script.append("		while (('crdownload'.equals(fileExtension) || 'tmp'.equals(fileExtension)) && maxCounter-- > 0) {\n");
+		script.append("			Thread.sleep(500);\n");
+		script.append("			downloadFilePath = getLastModifiedFileName();\n");
+		script.append("			fileExtension = org.apache.commons.io.FilenameUtils.getExtension(downloadFilePath);\n");
+		script.append("		}\n");
+		script.append("		if(maxCounter == 0) return 'timeout';\n");
+		script.append("		return downloadFilePath;\n");
+		script.append("}\n");
+		script.append("return 'not_started';\n");
+
+		String res = (String) Script.executeGroovyScript(script.toString());
+
+		if (res.equals("not_started")) {
+			throw new RuntimeException("Download file error. Downloading of file hadn't started.");
+		} else if (res.equals("timeout")) {
+			throw new RuntimeException("Download file error. Downloading of file was processing too long.");
+		}
+
+		return res;
+	}
+}
---
+++ src/main/java/com/ibagroup/wf/intelia/core/system/WebPageObject.java
@@ -0,0 +1,91 @@
+package com.ibagroup.wf.intelia.core.system;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ibagroup.wf.intelia.core.config.ConfigurationManager;
+import com.workfusion.rpa.helpers.RPA;
+
+import groovy.lang.Binding;
+
+public class WebPageObject extends PageObject {
+
+	private static final Logger logger = LoggerFactory.getLogger(WebPageObject.class);
+
+	public WebPageObject(Binding binding, ConfigurationManager cmn) {
+		super(binding, cmn);
+	}
+
+	public boolean isElementDisplayed(By elementLocator) {
+		return isElementDisplayed(elementLocator, null);
+	}
+
+	public boolean isElementDisplayed(By elementLocator, WebElement relatedTo) {
+		try {
+			getDriver().manage().timeouts().implicitlyWait(200, TimeUnit.MICROSECONDS);
+			List<WebElement> elements = (relatedTo != null ? relatedTo : getDriver()).findElements(elementLocator);
+			return elements.stream().anyMatch(e -> e.isDisplayed());
+		} catch (UnhandledAlertException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(String.format("Cannot find element using '%s' because an error occured.", elementLocator), e);
+			return false;
+		} finally {
+			getDriver().manage().timeouts().implicitlyWait(DEFAULT_IMPLICITLY_WAIT_TIMEOUT, TimeUnit.SECONDS);
+		}
+	}
+
+	public void scrollToElement(WebElement element) {
+		StringBuilder script = new StringBuilder();
+		script.append("var el = arguments[0]; \n");
+		script.append("el.scrollIntoView({block: 'center', inline: 'center'}); \n");
+		script.append("var hScrollOffset = el.getBoundingClientRect().left; \n");
+		script.append("while(el && el.scrollWidth == el.clientWidth){ \n");
+		script.append("		el = el.parentElement;\n");
+		script.append("} \n");
+		script.append("if(el){ \n");
+		script.append("		el.scrollLeft = hScrollOffset; \n");
+		script.append("}");
+		getDriver().executeScript(script.toString(), element);
+	}
+
+	public void zoom(double factor) {
+		getDriver().executeScript("document.body.style.zoom = arguments[0];", factor);
+	}
+
+	public InputStream takeScreenshot(Double zoomFactor) {
+		try {
+			if (zoomFactor != null) {
+				zoom(zoomFactor);
+			}
+			BufferedImage image = RPA.screenshotAsImage();
+			if (zoomFactor != null) {
+				zoom(1);
+			}
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			try {
+				ImageIO.write(image, "PNG", baos);
+				baos.flush();
+				byte[] imageInByte = baos.toByteArray();
+				return new ByteArrayInputStream(imageInByte);
+			} finally {
+				baos.close();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("Taking of screenshot has failed.", e);
+		}
+	}
+}
